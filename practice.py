920c5fb94b008f706821d0e9c23f93be9afab8f4
Deepak044 <deepakpanjwani430@gmail.com>
2026-08-26 13:10:42 -0400

    Changes in practice file

--- practice.py
+++ practice.py
@@ -1,1 +1,25 @@
-print("Hello World")
+# print("Hello World")
+
+# alphabets = "ABCDE"
+# for i in alphabets:
+#     print(i)
+
+# String Methods
+
+# a = "dEEpak"
+# print(a)
+# print(a.upper())
+# print(a.lower())
+# print(a.capitalize())
+# print(a.replace("dEEpak", "John"))
+
+str1 = "Welcome to the console!!"
+print(len(str1))
+print((str1.center(50)))
+print(len(str1.center(50)))
+
+print(str1.count("e"))
+print(str1.endswith("!"))
+print(str1.find("to", 4, 10))    # find will check the given value and return the index, if value is absent it will return -1.
+
+
