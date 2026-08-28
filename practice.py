828f700b15101412478d67d3792100368a03cbba
Deepak044 <deepakpanjwani430@gmail.com>
2026-08-28 17:17:50 -0400

    Updated file

--- practice.py
+++ practice.py
@@ -13,7 +13,8 @@
 # print(a.capitalize())
 # print(a.replace("dEEpak", "John"))
 
-str1 = "Welcome to the console!!"
+# str1 = "Welcome to the console"
+str1 = "Deepak"
 print(len(str1))
 print((str1.center(50)))
 print(len(str1.center(50)))
@@ -21,5 +22,11 @@ print(len(str1.center(50)))
 print(str1.count("e"))
 print(str1.endswith("!"))
 print(str1.find("to", 4, 10))    # find will check the given value and return the index, if value is absent it will return -1.
+
+print(str1.isalnum())            # Alpha numeric: Return True if string consist of A-Z, a-z,0-9, and if other punctuations are present it returns false.
+print(str1.isalpha())            # isalpha() : Return True if string consist of A-Z, a-z, and if other punctuations or numbers are present it returns false.
+
+print(str1.islower())            # islower() - Returns True if all characters are in lowercase, else False.
+
 
 
