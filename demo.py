0bef649c680985ffe218345a267215d0e09ffaac
Deepak044 <deepakpanjwani430@gmail.com>
2026-09-01 16:36:31 -0400

    Change in demo file

--- demo.py
+++ demo.py
@@ -1,5 +1,13 @@
-fruits = ["Apple", "Banana", "Mango"]
 
-print(type(fruits))
-
-
+import time
+hour=int(time.strftime('%H'))
+mint=int(time.strftime('%M'))
+print(f"The current time is {hour}:{mint}")
+if hour>=0 and hour<12 :
+    print("Good morning");
+elif hour>=12 and hour<17 : 
+    print("Good afternoon");
+elif hour>=17 and hour<19: 
+    print("Good evening")
+else:
+    print("Good night")
